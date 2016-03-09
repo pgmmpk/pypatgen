a5afafd51cc9d3ae567c20eb514b27ece9068d92
Mike Kroutikov <pgmmpk@gmail.com>
2016-03-09 08:16:44 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pypatgen',
-    version='0.2.1',
+    version='0.2.2',
     description='TeX hyphenation pattern generator',
 
     url='https://github.com/pgmmpk/pypatgen',
